d2020642181340af8c831cd169e5c3855603d838
baljot <baljotsinghchoudhary@gmail.com>
2021-04-13 12:07:14 +0530

    by baljot

--- cloudAgri/settings/prod.py
+++ cloudAgri/settings/prod.py
@@ -1,9 +1,8 @@
 
-import os
+import os,platform
+import environ,json,dj_database_url
 from pathlib import Path
-import dj_database_url
-import environ
-import json
+
 
 
 environ.Env.read_env()
@@ -11,6 +10,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+PYTHON_BITNESS, LINKAGE = platform.architecture()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
